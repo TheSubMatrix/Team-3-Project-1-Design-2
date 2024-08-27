a17fbb7e7660545d3109b6806cea379af60b588a
DeathToRightz <142912611+DeathToRightz@users.noreply.github.com>
2024-08-26 23:14:46 -0400

    Big update
    Added art assets to demo scene, fixed menus, added crowbar script to crowbar

--- Assets/Scripts/UI/UI Manager.cs
+++ Assets/Scripts/UI/UI Manager.cs
@@ -6,23 +6,13 @@ using TMPro;
 
 public class UIManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+   
 
 
     public void WhenStartButtonisClicked()
     {
 
-        SceneManager.LoadScene("Test Scene");
+        SceneManager.LoadScene("MileStone1");
 
     }
 
