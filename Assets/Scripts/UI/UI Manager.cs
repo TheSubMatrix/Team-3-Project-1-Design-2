1878079e717e3cc2d6e8e0665e1e891659688af2
DeathToRightz <142912611+DeathToRightz@users.noreply.github.com>
2024-09-04 21:31:57 -0400

    Added sounds
    Main menu music with button click sounds

--- Assets/Scripts/UI/UI Manager.cs
+++ Assets/Scripts/UI/UI Manager.cs
@@ -3,41 +3,62 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
-
 public class UIManager : MonoBehaviour
 {
 
+    private AudioSource audioSource;
+    [SerializeField] AudioClip buttonClickSFX;
 
    
 
+    private void Awake()
+    {
+        
+        audioSource = GetComponent<AudioSource>();
+    }
+    private void Start()
+    {
+        audioSource.Play();   
+    }
+
+    private void Update()
+    {
+     if(SceneManager.GetActiveScene().name == "LevelOne")
+        {
+            Destroy(this);
+        }   
+    }
     public void WhenStartButtonisClicked()
     {
-        SceneTransition.Instance.ChangeScene(1,4,"Level One");
+        audioSource.PlayOneShot(buttonClickSFX);
+        SceneTransition.Instance.ChangeScene(1,5,"Level One");
        // SceneManager.LoadScene("MileStone1");
 
     }
 
     public void WhenHelpButtonisClicked()
     {
-
+        audioSource.PlayOneShot(buttonClickSFX);
         SceneTransition.Instance.ChangeScene(1,1,"Help");
        // SceneManager.LoadScene("Help");
 
     }
     public void WhenCreditsButtonisClicked()
     {
-       // SceneManager.LoadScene("Credits");
+        // SceneManager.LoadScene("Credits");
+        audioSource.PlayOneShot(buttonClickSFX);
         SceneTransition.Instance.ChangeScene(1,1,"Credits");
     }
     public void WhenReplayButtonisClicked()
     {
+        audioSource.PlayOneShot(buttonClickSFX);
         SceneTransition.Instance.ChangeScene(1,1,"MainMenu");
         //SceneManager.LoadScene("MainMenu");
 
     }
     public void WhenQuitButtonisClicked()
     {
-
+        audioSource.PlayOneShot(buttonClickSFX);
         Application.Quit();
 
         
@@ -48,6 +69,7 @@ public class UIManager : MonoBehaviour
 
     public void WhenBackButtonisClicked()
     {
+        audioSource.PlayOneShot(buttonClickSFX);
         SceneTransition.Instance.ChangeScene(1,1,"MainMenu");
        // SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
