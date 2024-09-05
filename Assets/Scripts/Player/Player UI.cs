644a53a2096ba664020cd843fb1655bb8da0b7d8
DeathToRightz <142912611+DeathToRightz@users.noreply.github.com>
2024-09-05 18:56:22 -0400

    Update

--- Assets/Scripts/Player/Player UI.cs
+++ Assets/Scripts/Player/Player UI.cs
@@ -14,40 +14,45 @@ public class PlayerUI : MonoBehaviour
 
     private void Start()
     {
-        StartCoroutine(FadeOutPlayerUI(playerUIImages[0], 1, 0, 5, 10f));
+        
     }
 
 
     IEnumerator FadeOutPlayerUI(Image image, int startAlpha, int endAlpha, float duration, float delay)
     {
         float elapsedTime = 0f;
-        elapsedTime += Time.deltaTime;
-        Debug.Log(elapsedTime);
+        float delayHolder = 0f;      
         Color imageColor = image.color;
-        if(elapsedTime > delay)
+       
+        while(delayHolder < delay)
         {
+            delayHolder += Time.deltaTime;
+            Debug.Log(delayHolder);
+            yield return null;
+        }
+       
             while (elapsedTime < duration)
             {
-               
+                elapsedTime += Time.deltaTime;
                 Debug.Log(elapsedTime);
                 imageColor.a = Mathf.Lerp(startAlpha, endAlpha, elapsedTime / duration);
                 image.color = imageColor;
                 yield return null;
             }
-        }
+       
         
     }
 
 
-    void HidePlayerIU(Image image, int startAlpha, int endAlhpa, float duration, bool fadeIn)
+    void HidePlayerUI(Image image, bool fadeIn, float delay)
     {
        if(fadeIn)
         {
-            StartCoroutine(FadeOutPlayerUI(image, 0, 1, 5f, 10f));
+            StartCoroutine(FadeOutPlayerUI(image, 0, 1, 5f, delay));
         }
         else
         {
-            StartCoroutine(FadeOutPlayerUI(image, 1, 0, 5f, 10f));
+            StartCoroutine(FadeOutPlayerUI(image, 1, 0, 5f, delay));
         }
         
     }
