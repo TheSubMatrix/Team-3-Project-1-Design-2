db4e7a33af5bdd3eb3cd784e6d35609415aadcd5
Colin Whiteford <colinwhiteford516@gmail.com>
2024-09-10 02:02:44 -0400

    Fixed PBR Shader issues and added channel for journals to call to the watch for enabling

--- Assets/Scripts/Interactable/Paper Interacable.cs
+++ Assets/Scripts/Interactable/Paper Interacable.cs
@@ -3,20 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 
-public class PaperInteracable : MonoBehaviour, IInteractable
+public class PaperInteractable : MonoBehaviour, IInteractable
 {
+    [SerializeField] SO_VoidChannel watchEnableChannel;
     [SerializeField] SO_ImageDisplayChannel.ImageDisplayInfo imageDisplayInfoEnable;
     [SerializeField] SO_ImageDisplayChannel.ImageDisplayInfo imageDisplayInfoDisable;
     [SerializeField] SO_ImageDisplayChannel uiPopupChannel;
     [SerializeField] SO_ImageDisplayChannel imageDisplayInfoShow;
     public PlayerInteractionHandler interactionHandler { get; set; }
     public bool ShouldStopMovement { get; set; } = true;
+
     public void OnInteractStart(PlayerInteractionHandler incomingHandler)
     {
         interactionHandler = incomingHandler;
         Debug.Log("Paper Picked Up");
         uiPopupChannel.OnFadeImage.Invoke(new SO_ImageDisplayChannel.ImageDisplayInfo("Journal Pages", 0, .8f, .5f, 0));
         imageDisplayInfoShow.OnFadeImage?.Invoke(imageDisplayInfoEnable);
+        watchEnableChannel.myEvent?.Invoke();
     }
     public void OnInteracting()
     {
